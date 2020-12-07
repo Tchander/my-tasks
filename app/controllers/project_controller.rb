ceec8a414115308497553e913c2123a936a7fab7
Tchander <evgeniy.tchander@yandex.ru>
2020-12-07 13:09:20 +0300

    master:feat:add right routes for get, post and patch

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,7 +1,6 @@
 class ProjectController < ApplicationController
 
   def index
-    # render json: :ok
     @projects = Project.all
     projects = @projects.map do |p|
       ProjectSerializer.new(p).as_json
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :project
+  resources :todo
   root 'project#index'
   get '/projects', to: 'project#index'
+  post '/todos', to: 'todo#create'
+  patch '/projects/:id/todo/:todo_id', to: 'todo#update'
 end
