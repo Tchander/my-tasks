facf095012f2577ac8c041c595e22083ab1efb55
Tchander <evgeniy.tchander@yandex.ru>
2020-12-10 11:58:02 +0300

    master:minor imrovements

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,15 +1,16 @@
 class TodoController < ApplicationController
 
   def create
-    @project = Project.find_by(:title, params[:title])
+    @project = Project.find_by(title: params[:title])
     if @project
-      @project.todos.build(params[:text])
+      @project.todos.build(text: params[:text])
       @project.save
     else
-      @project = Project.new(params[:title])
-      @project.todos.build(params[:text])
+      @project = Project.new(title: params[:title])
+      @project.todos.build(text: params[:text])
       @project.save
     end
+    render json: @project
   end
 
   def update
